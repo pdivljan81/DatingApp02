206773bc14020321244634d8a1f53f865dea4775
pdivljan81 <pdivljan@gmail.com>
2021-10-27 22:11:29 +0200

    End od 56 lesson

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -10,17 +10,15 @@ import { User } from '../_models/user';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-loggedIn: boolean = false;  
   constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+   
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     })
@@ -28,15 +26,5 @@ loggedIn: boolean = false;
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
-  }
-
-  getCurrentUser(){
-    this.accountService.currentUser$.subscribe(user=> {
-      this.loggedIn = !!user;
-    },error => {
-      console.log(error);
-    }
-    )
   }
 }
